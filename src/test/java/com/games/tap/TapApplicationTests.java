54277cc1ba6d2d006abe5285e137b040ffd9dc2f
sea_of_light <327404055@qq.com>
2022-06-29 22:22:59 +0800

    init cloud

--- src/test/java/com/games/tap/TapApplicationTests.java
+++ src/test/java/com/games/tap/TapApplicationTests.java
@@ -20,58 +20,58 @@ import java.io.IOException;
 @SpringBootTest
 @Slf4j
 class TapApplicationTests {
-	@Resource
-	SearchService service;
-	@Resource
-	ElasticsearchClient client;
-	@Test
-	void test() {
-		User u1=new User(4L,"塞尔达玩家","111");
-		User u2=new User(1L,"原皮魅力时刻","999");
-		User u3=new User(3L,"正常玩家","woc,原");
-		try {
-//			service.addItem("user",u1);
-//			service.updateItem("user",u2,User.class);
-//			service.addUser(u2);
-//			service.addUser(u3);
-			service.searchUser("魅力",0,10);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	@Test
-	void test1(){
-		try {
-			service.searchAll("user",0,10,User.class);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	@Test
-	void test2(){
-		try {
-//			service.deleteUser("1");
-			service.deleteIndex("user");
-			service.createUserIndex();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	@Test
-	void test3(){
-		try {
-			log.info(service.getItem("user","1",User.class).toString());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	@Test
-	void test4(){
-		try {
-			service.createUserIndex();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+	// @Resource
+	// SearchService service;
+	// @Resource
+	// ElasticsearchClient client;
+// 	@Test
+// 	void test() {
+// 		User u1=new User(4L,"塞尔达玩家","111");
+// 		User u2=new User(1L,"原皮魅力时刻","999");
+// 		User u3=new User(3L,"正常玩家","woc,原");
+// 		try {
+// //			service.addItem("user",u1);
+// //			service.updateItem("user",u2,User.class);
+// //			service.addUser(u2);
+// //			service.addUser(u3);
+// 			service.searchUser("魅力",0,10);
+// 		} catch (IOException e) {
+// 			e.printStackTrace();
+// 		}
+// 	}
+// 	@Test
+// 	void test1(){
+// 		try {
+// 			service.searchAll("user",0,10,User.class);
+// 		} catch (IOException e) {
+// 			e.printStackTrace();
+// 		}
+// 	}
+// 	@Test
+// 	void test2(){
+// 		try {
+// //			service.deleteUser("1");
+// 			service.deleteIndex("user");
+// 			service.createUserIndex();
+// 		} catch (IOException e) {
+// 			e.printStackTrace();
+// 		}
+// 	}
+// 	@Test
+// 	void test3(){
+// 		try {
+// 			log.info(service.getItem("user","1",User.class).toString());
+// 		} catch (IOException e) {
+// 			e.printStackTrace();
+// 		}
+// 	}
+// 	@Test
+// 	void test4(){
+// 		try {
+// 			service.createUserIndex();
+// 		} catch (IOException e) {
+// 			e.printStackTrace();
+// 		}
+// 	}
 
 }
